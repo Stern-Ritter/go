6e886662a6a54198fce45311470357c03d6dfc1d
Stern_Ritter <fadeef.k@yandex.ru>
2024-04-18 18:30:53 +0300

    refactor: refactor code to pass linters

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -1,15 +1,14 @@
 package printer
 
 import (
-	"github.com/fixme_my_friend/hw02_fix_app/types"
+	"fmt"
+
+	"github.com/Stern-Ritter/go/hw02_fix_app/types"
 )
 
-func PrintStaff(staff types.Employee) {
-	var str string
-	for i := 0; i < len(staff); i++ {
-		str := fmt.Sprintf("User ID: %d; Age: %d; Name: %s; Department ID: %d; ", staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
-		fmt.Println(str)
+func PrintStaff(employees []types.Employee) {
+	for _, employee := range employees {
+		fmt.Printf("User ID: %d; Age: %d; Name: %s; Department ID: %d; \n",
+			employee.UserID, employee.Age, employee.Name, employee.DepartmentID)
 	}
-
-	fmt.Println(str)
 }
