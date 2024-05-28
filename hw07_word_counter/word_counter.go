faff8eb3dc5100ce2a400dd742e34c299f8bc7dc
Stern_Ritter <fadeef.k@yandex.ru>
2024-05-28 19:32:17 +0300

    feat: add countWords func

--- hw07_word_counter/word_counter.go
+++ hw07_word_counter/word_counter.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"regexp"
+	"strings"
+)
+
+func countWords(input string) map[string]int {
+	if len(input) == 0 {
+		return make(map[string]int)
+	}
+
+	reg := regexp.MustCompile(`[^\pL\d\s\t\n]+`)
+	fmtInput := reg.ReplaceAllString(input, "")
+
+	words := make(map[string]int)
+	for _, word := range strings.Fields(fmtInput) {
+		words[word]++
+	}
+	return words
+}
