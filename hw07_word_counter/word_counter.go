d70eab6c9350997a0b4fa34691e058ee677e211f
Stern_Ritter <fadeef.k@yandex.ru>
2024-06-03 13:24:21 +0300

    fix: fix after review

--- hw07_word_counter/word_counter.go
+++ hw07_word_counter/word_counter.go
@@ -5,13 +5,14 @@ import (
 	"strings"
 )
 
+var wordsFilterRegex = regexp.MustCompile(`[^\pL\d\s\t\n]+`)
+
 func countWords(input string) map[string]int {
 	if len(input) == 0 {
 		return make(map[string]int)
 	}
 
-	reg := regexp.MustCompile(`[^\pL\d\s\t\n]+`)
-	fmtInput := reg.ReplaceAllString(input, "")
+	fmtInput := wordsFilterRegex.ReplaceAllString(input, "")
 
 	words := make(map[string]int)
 	for _, word := range strings.Fields(fmtInput) {
