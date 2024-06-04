40c822a0678fd0eead6d6111e95f6e3239bad2a6
Stern_Ritter <fadeef.k@yandex.ru>
2024-06-04 10:17:33 +0300

    fix: fix linter errors

--- hw08_binary_search/binary_search_test.go
+++ hw08_binary_search/binary_search_test.go
@@ -14,19 +14,19 @@ func TestBinarySearchInt64(t *testing.T) {
 		expectedPosition int
 	}{
 		{
-			name:             "should find target element and return his position when target element is on the first position in the array ",
+			name:             "should find target element and return position when target element is on the first position",
 			arr:              []int64{1, 2, 3, 4, 5},
 			target:           1,
 			expectedPosition: 0,
 		},
 		{
-			name:             "should find target element an return his position when target element is on the last position in the array ",
+			name:             "should find target element an return position when target element is on the last position",
 			arr:              []int64{1, 2, 3, 4, 5},
 			target:           5,
 			expectedPosition: 4,
 		},
 		{
-			name:             "should find target element and return his position when target element is on the middle position in the array ",
+			name:             "should find target element and return position when target element is on the middle position",
 			arr:              []int64{1, 2, 3, 4, 5},
 			target:           3,
 			expectedPosition: 2,
@@ -67,19 +67,19 @@ func TestBinarySearchFloat64(t *testing.T) {
 		expectedPosition int
 	}{
 		{
-			name:             "should find target element and return his position when target element is on the first position in the array ",
+			name:             "should find target element and return position when target element is on the first position",
 			arr:              []float64{0.1, 0.2, 0.3, 0.4, 0.5},
 			target:           0.1,
 			expectedPosition: 0,
 		},
 		{
-			name:             "should find target element an return his position when target element is on the last position in the array ",
+			name:             "should find target element an return position when target element is on the last position",
 			arr:              []float64{0.1, 0.2, 0.3, 0.4, 0.5},
 			target:           0.5,
 			expectedPosition: 4,
 		},
 		{
-			name:             "should find target element and return his position when target element is on the middle position in the array ",
+			name:             "should find target element and return position when target element is on the middle position",
 			arr:              []float64{0.1, 0.2, 0.3, 0.4, 0.5},
 			target:           0.3,
 			expectedPosition: 2,
@@ -120,19 +120,19 @@ func TestBinarySearchString(t *testing.T) {
 		expectedPosition int
 	}{
 		{
-			name:             "should find target element and return his position when target element is on the first position in the array ",
+			name:             "should find target element and return position when target element is on the first position",
 			arr:              []string{"abc", "bcd", "cde", "def", "efg"},
 			target:           "abc",
 			expectedPosition: 0,
 		},
 		{
-			name:             "should find target element an return his position when target element is on the last position in the array ",
+			name:             "should find target element an return position when target element is on the last position",
 			arr:              []string{"abc", "bcd", "cde", "def", "efg"},
 			target:           "efg",
 			expectedPosition: 4,
 		},
 		{
-			name:             "should find target element and return his position when target element is on the middle position in the array ",
+			name:             "should find target element and return position when target element is on the middle position",
 			arr:              []string{"abc", "bcd", "cde", "def", "efg"},
 			target:           "cde",
 			expectedPosition: 2,
